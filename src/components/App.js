5fe4bb047cc48000c777081d0f71024d52a0acba
sergiodiezdepedro <sergio.d.depedro@gmail.com>
2018-03-29 20:38:21 +0200

    Finalizado Boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div class="container">
+                <h1>React App Boilerplate</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
